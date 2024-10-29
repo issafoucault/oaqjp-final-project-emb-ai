ab508920fbe61f417e6436512140ab9bf407708d
issafoucault <alejandro.foucault@issprops.com>
2024-10-29 14:56:37 -0400

    add error handling.

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -1,12 +1,16 @@
-import requests # Import the requests library to handle HTTP requests
-import json
-def emotion_detector(text_to_analyse): # Define a function named emotion_detector that takes a string input (text_to_analyse)
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
-    input_json = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
-    response = requests.post(URL, json = input_json, headers=header)  # Send a POST request to the API with the text and headers
-    #return response.text  # Return the response text from the API
+"""
+This is the emotion_detector() function using Watson NLP library.
+"""
 
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    
     # Step 1: Convert the response text into a dictionary
     formatted_response = json.loads(response.text)
 
@@ -24,6 +28,7 @@ def emotion_detector(text_to_analyse): # Define a function named emotion_detecto
         emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
         dominant_emotion_key = emotion_keys[dominant_emotion_index]
 
+    # Add status code error handling
     elif response.status_code == 400:
         anger_score = None
         disgust_score = None
